2993576c2cba0242a985c9bd2efc6b93c7958032
jeonjeongbin <01jjb@naver.com>
2025-07-11 10:52:22 +0900

    프로젝트 폴더 구조 잡기

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,15 +1,7 @@
-import Footer from "./Main Page/components/layout/Footer";
-import Header from "./Main Page/components/layout/Header";
-import Main from "./Main Page/components/layout/Main";
-import SideBar from "./Main Page/components/layout/SideBar";
-
 function App() {
   return (
     <>
-    <Header />
-    <SideBar />
-    <Main />
-    <Footer />
+
     </>
   );
 }
