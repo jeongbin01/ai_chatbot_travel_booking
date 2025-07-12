d11fcf0fe3f38ce5d683b40ec0945bc3499c64bc
jeongbin <01jjb@naver.com>
2025-07-12 15:48:15 +0900

    Header - 완료

--- FE/src/components/main/Header.jsx
+++ FE/src/components/main/Header.jsx
@@ -1,62 +1,65 @@
-import React from 'react'
-import logo from '../../assets/images/On_Comma.png';
-import '../../styles/layout/Header.css'; 
+import React from 'react';
+import logo from '../../assets/images/On_Comma.png'; // 실제 경로에 맞게 조정
+import '../../styles/layout/Header.css'; // 스타일 파일 연결
 
-function Header() {
+const Header = () => {
   return (
-    <header className="header">
-      <div className="header-container">
-        {/* 왼쪽: 로고 + 햄버거 */}
-        <div className="left-section">
-          <img src={logo} alt="OnComma Logo" className="logo" />
+    <header className="header-wrapper">
+
+      {/* 🔹 탑 메뉴 (로그인/회원가입/고객센터) */}
+      <div className="top-menu">
+        <div className="container right-align">
+          <ul className="top-links">
+            <li><a href="/login">로그인</a></li>
+            <li><a href="/signup">회원가입</a></li>
+            <li><a href="/support">고객센터</a></li>
+          </ul>
+        </div>
+      </div>
+
+      {/* 🔹 로고 + 메인 메뉴 + 아이콘 메뉴 */}
+      <div className="main-header container">
+        {/* 로고 */}
+        <div className="logo-area">
+          <a href="/">
+            <img src={logo} alt="OnComma Logo" />
+          </a>
         </div>
 
-        {/* 가운데: 메뉴 */}
-        <nav className="center-section">
-          <ul className="menu-list">
-            <li>
-              <span>국내여행</span>
-            </li>
-            <li>
-              <span>해외여행</span>
-            </li>
-            <li>
-              <span>숙소</span>
-            </li>
-            <li>
-              <span>항공권</span>
-            </li>
-            <li>
-              <span>렌트카</span>
-            </li>
-            <li>
-              <span>액티비티</span>
-            </li>
+        {/* 메인 네비게이션 */}
+        <nav className="nav-menu">
+          <ul>
+            <li><a href="/accommodations?region=korea">국내 숙소</a></li>
+            <li><a href="/accommodations?region=global">해외 숙소</a></li>
+            <li><a href="/products">여행 상품</a></li>
+            <li><a href="/products?category=항공권">항공권 예약</a></li>
+            <li><a href="/products?category=렌터카">렌터카 예약</a></li>
+            <li><a href="/mypage">마이페이지</a></li>
           </ul>
         </nav>
 
-        {/* 왼쪽: 메뉴 */}
-        <ul className="right-section">
-          <li>
-            <span>로그인／회원가입</span>
-          </li>
-          <li>
-            <i className="bi bi-person-circle" />
-            <span>My</span>
-          </li>
-          <li>
-            <i className="bi-calendar-check" />
-            <span>예약내역</span>
-          </li>
-          <li>
-            <i className="bi bi-ticket-detailed-fill" />
-            <span>이벤트/쿠폰</span>
-          </li>
-        </ul>
+        {/* 아이콘 메뉴 */}
+        <div className="icon-menu">
+          <a href="/wishlist" title="찜하기">
+            <i className="bi bi-heart"></i>
+          </a>
+          <a href="/notifications" title="알림">
+            <i className="bi bi-bell"></i>
+          </a>
+          <a href="/cart" title="장바구니">
+            <i className="bi bi-cart3"></i>
+          </a>
+          <a href="/profile" title="프로필">
+            <i className="bi bi-person-circle"></i>
+          </a>
+          <a href='/' title='햄버거 메뉴'>
+            <i className="bi bi-list"></i>
+          </a>
+        </div>
       </div>
+
     </header>
+  );
+};
 
-  )
-}
-
-export default Header
+export default Header;
