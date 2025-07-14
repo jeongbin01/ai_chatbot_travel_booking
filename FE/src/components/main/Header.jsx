1c712eefe2f6f677f1325a89369a7b4e1456f33c
jeonjeongbin <01jjb@naver.com>
2025-07-14 15:51:47 +0900

    Header - 수정 완료

--- FE/src/components/main/Header.jsx
+++ FE/src/components/main/Header.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import logo from '../../assets/images/On_Comma.png'; // 실제 경로에 맞게 조정
-import '../../styles/layout/Header.css'; // 스타일 파일 연결
+import logo from '../../assets/images/On_Comma.png';
+import '../../styles/layout/Header.css'; 
 
 const Header = () => {
   return (
     <header className="header-wrapper">
 
-      {/* 🔹 탑 메뉴 (로그인/회원가입/고객센터) */}
+      {/* 탑 메뉴 */}
       <div className="top-menu">
         <div className="container right-align">
           <ul className="top-links">
             <li><a href="/login">로그인</a></li>
             <li><a href="/signup">회원가입</a></li>
+            <li><a href="/company">고객센터</a></li>
           </ul>
         </div>
       </div>
 
-      {/* 🔹 로고 + 메인 메뉴 + 아이콘 메뉴 */}
       <div className="main-header container">
         {/* 로고 */}
         <div className="logo-area">
@@ -30,10 +30,9 @@ const Header = () => {
           <ul>
             <li><a href="/accommodations?region=korea">국내 숙소</a></li>
             <li><a href="/accommodations?region=global">해외 숙소</a></li>
-            <li><a href="/products">여행 상품</a></li>
-            <li><a href="/products?category=항공권">항공권 예약</a></li>
-            <li><a href="/products?category=렌터카">렌터카 예약</a></li>
-            <li><a href="/mypage">마이페이지</a></li>
+            <li><a href="/products?category=항공권">항공</a></li>
+            <li><a href="/products?category=렌터카">렌터카</a></li>
+            <li><a href="/mypage"> 패키지 여행 </a></li>
           </ul>
         </nav>
 
@@ -45,18 +44,14 @@ const Header = () => {
           <a href="/notifications" title="알림">
             <i className="bi bi-bell"></i>
           </a>
-          <a href="/cart" title="장바구니">
-            <i className="bi bi-cart3"></i>
-          </a>
-          <a href="/profile" title="프로필">
+          <a href="/profile" title="My">
             <i className="bi bi-person-circle"></i>
           </a>
-          <a href='/' title='햄버거 메뉴'>
+          <a href='/menu' title='햄버거 메뉴'>
             <i className="bi bi-list"></i>
           </a>
         </div>
       </div>
-
     </header>
   );
 };
