6355f62d0fe193f771da0729dc6cddb8fca8da7c
jeongbin <01jjb@naver.com>
2025-08-08 21:41:23 +0900

    Amenity - 오타수정

--- BE/TravelProject/src/main/java/com/example/TravelProject/controller/room/AmenityController.java
+++ BE/TravelProject/src/main/java/com/example/TravelProject/controller/room/AmenityController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/ap/amenities")
+@RequestMapping("/app/amenities")
 @RequiredArgsConstructor
 public class AmenityController {
 
