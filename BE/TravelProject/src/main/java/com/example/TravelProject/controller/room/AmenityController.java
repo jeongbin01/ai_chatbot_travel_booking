920b329d05deba12b58e96dfccbdba9700891fd8
jeongbin <01jjb@naver.com>
2025-08-08 11:33:29 +0900

    Amenity - 구현 완료

--- BE/TravelProject/src/main/java/com/example/TravelProject/controller/room/AmenityController.java
+++ BE/TravelProject/src/main/java/com/example/TravelProject/controller/room/AmenityController.java
@@ -1,41 +1,39 @@
-//package com.example.TravelProject.controller.room;
-//
-//import com.example.TravelProject.entity.room.Amenity;
-//import com.example.TravelProject.service.Room.AmenityService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@RestController
-//@RequestMapping("/api/amenities")
-//@RequiredArgsConstructor
-//public class AmenityController {
-//
-//    private final AmenityService amenityService;
-//
-//    // 모든 편의시설 조회
-//    @GetMapping
-//    public List<Amenity> getAllAmenities() {
-//        return amenityService.findAll();
-//    }
-//
-//    // 편의시설 ID로 단건 조회
-//    @GetMapping("/{id}")
-//    public Optional<Amenity> getAmenityById(@PathVariable Integer id) {
-//        return amenityService.findById(id);
-//    }
-//
-//    // 새로운 편의시설 등록
-//    @PostMapping
-//    public Amenity createAmenity(@RequestBody Amenity amenity) {
-//        return amenityService.save(amenity);
-//    }
-//
-//    // 편의시설 삭제
-//    @DeleteMapping("/{id}")
-//    public void deleteAmenity(@PathVariable Integer id) {
-//        amenityService.deleteById(id);
-//    }
-//}
+package com.example.TravelProject.controller.room;
+
+import com.example.TravelProject.entity.room.Amenity;
+import com.example.TravelProject.service.Room.AmenityService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/ap/amenities")
+@RequiredArgsConstructor
+public class AmenityController {
+
+    private final AmenityService amenityService;
+
+    // 모든 편의시설 조회
+    @Operation(
+        summary = "편의시설 전체 조회",
+        description = "등록된 모든 편의시설 목록을 반환합니다."
+    )
+    @GetMapping
+    public List<Amenity> getAllAmenities() {
+        return amenityService.findAll();
+    }
+
+    // 편의시설 ID로 단건 조회
+    @Operation(
+            summary = "편의시설 단건 조회",
+            description = "amenityId로 특정 편의시설을 조회합니다."
+        )
+    @GetMapping("/{id}")
+    public Optional<Amenity> getAmenityById(@PathVariable Integer id) {
+        return amenityService.findById(id);
+    }
+}
