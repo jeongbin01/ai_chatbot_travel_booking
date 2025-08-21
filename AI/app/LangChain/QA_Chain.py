a165d1a8d587e86b9e48727a06987c42277dd0f0
osm3735 <hdosm3735@gmail.com>
2025-08-21 15:29:50 +0900

    AI 랭체인 구현

--- AI/app/LangChain/QA_Chain.py
+++ AI/app/LangChain/QA_Chain.py
@@ -3,8 +3,11 @@ from langchain.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from pydantic import BaseModel, Field
 from langchain_core.output_parsers import JsonOutputParser
+from Domain.tourAPI import TourApiDomain
 import yaml
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class Answer(BaseModel):
     title_en: str = Field(description="")
@@ -14,10 +17,10 @@ class Answer(BaseModel):
     urls: str = Field(description="")
 
 class QandAChain:
-    def __init__(self, question):
+    def __init__(self):        
         base_path = os.path.dirname(os.path.abspath(__file__))  # 현재 파일의 경로
         file_path = os.path.join(base_path, 'prompt', 'QandAPrompt.yaml')
-
+        self.__openaiApiKey = os.environ["OPENAI_API_KEY"]
         with open(file_path, 'r') as f:
             self.__prompt_template = yaml.load(f, Loader=yaml.SafeLoader)['template']
         
@@ -25,15 +28,21 @@ class QandAChain:
             temperature = 0.1,
             model_name="gpt-4o-mini",
             max_tokens = 2048,
+            openai_api_key=self.__openaiApiKey 
         )
+        self.__tourapi = TourApiDomain()
         self.__parser = JsonOutputParser(pydantic_object=Answer)
 
     def main_chain_invoke(self, question):
         prompt = PromptTemplate.from_template(self.__prompt_template)
+        tourapiData = self.__tourapi.question_to_param(question)
+        
         prompt = prompt.partial(format_instructions=self.__parser.get_format_instructions())
 
         chain = prompt | self.__model | self.__parser
 
-        result = chain.invoke({"question": question})
+        result = chain.invoke({"question": question, "tourapiData":tourapiData})
 
+        # best_row1, best_row2 = tour_api_domain_instance.question_to_param(question)
+        # result = tourapiData
         return result
